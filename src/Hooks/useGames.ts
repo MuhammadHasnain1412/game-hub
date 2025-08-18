704ecedb7eb84dbdb159f03ace3ce0a36743aafe
MuhammadHasnain142002 <muhammad.hasnain142002@gmail.com>
2025-08-18 13:57:41 +0500

    Adding Critic Score

--- src/Hooks/useGames.ts
+++ src/Hooks/useGames.ts
@@ -3,21 +3,22 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
 export interface Platform {
-    id: number
-    name: string
-    slug: string
+  id: number;
+  name: string;
+  slug: string;
 }
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string
-    parent_platforms: {platform: Platform}[] 
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
 interface FetchGamesResponse {
-    count: number,
-    results: Game[]
+  count: number;
+  results: Game[];
 }
 
 const useGames = () => {
@@ -26,16 +27,17 @@ const useGames = () => {
   useEffect(() => {
     const controller = new AbortController();
     apiClient
-      .get<FetchGamesResponse>("/games", {signal: controller.signal})
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
       .catch((err) => {
-        if(err instanceof CanceledError) return;
-        setError(err.message)});
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
 
-    return () => controller.abort()  
-  },[]);
+    return () => controller.abort();
+  }, []);
 
   return { games, error };
 };
 
-export default useGames
+export default useGames;
