7eb574d973986ee63d896e3e51866e481e782457
MuhammadHasnain142002 <muhammad.hasnain142002@gmail.com>
2025-08-19 20:40:07 +0500

    Move interfaces to entities folder

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,6 +1,7 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import { BsChevronDown } from 'react-icons/bs'
-import usePlatforms,{ Platform} from '../Hooks/usePlatforms'
+import usePlatforms from '../Hooks/usePlatforms'
+import { Platform } from "../entities/Platform"
 import usePlatform from '../Hooks/usePlatform'
 import useGameQueryStore from '../store'
 
