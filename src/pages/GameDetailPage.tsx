c5f46f22f175f328d46cbcfe5cefdf8f33d32424
MuhammadHasnain142002 <muhammad.hasnain142002@gmail.com>
2025-08-20 01:57:29 +0500

    Build Game Attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,20 +1,24 @@
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import React from "react";
 import { useParams } from "react-router-dom";
+import CriticScore from "../components/CriticScore";
+import DefinitionItem from "../components/DefinitionItem";
 import ExpandableText from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttributes";
 import useGame from "../Hooks/useGame";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
 
-  if(isLoading) return <Spinner />
-  if(error || !game) throw error
+  if (isLoading) return <Spinner />;
+  if (error || !game) throw error;
   return (
     <div>
       <>
         <Heading>{game.name}</Heading>
         <ExpandableText>{game.description_raw}</ExpandableText>
+        <GameAttributes game={game}/>
       </>
     </div>
   );
