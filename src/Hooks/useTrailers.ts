f5840f8b4d97354a41fb42e5e05b5863647dd74e
MuhammadHasnain142002 <muhammad.hasnain142002@gmail.com>
2025-08-20 02:18:03 +0500

    Build Game Trailor

---
+++ src/Hooks/useTrailers.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import { Trailor } from "../entities/Trailor";
+import APIClient from "../services/api-client";
+
+const useTrailors = (gameId: number) => {
+    const apiClient = new APIClient<Trailor>(`/games/${gameId}/movies`)
+    return useQuery({
+    queryKey: ['trailors', gameId],
+    queryFn: apiClient.getAll
+})}
+
+export default useTrailors
